6a37cce3ff9d709b284f88d92ed7683e2bf5ab2e
Jhalaa <jhalaachinoy@gmail.com>
2018-04-23 09:31:59 -0700

    Adding more tests

--- test/test.js
+++ test/test.js
@@ -3,20 +3,35 @@ var test = true
 game = require('../js/game.js')
 
 describe('Basic testing for game.js', function () {
-    it('It sould have 5 distinct pokemon', function () {
+    it('It should have 5 distinct pokemon', function () {
       // console.log(game.Game)
       game.Game.generatePokemonIds()
       expect(game.pokemonIds.length).toEqual(5);
     });
 
-    it('It sould have 5 distinct pokemon positions', function () {
+    it('It should have 5 distinct pokemon positions', function () {
         game.Game.generatePokemonIds()
         expect(game.pokemonPositions.length).toEqual(5);
     });
 
-    it('It sould have 5 distinct pokemon positions', function () {
+    it('It should have 5 distinct pokemon positions', function () {
         game.Game.generatePokemonIds()
         expect(game.pokemonPositions.length).toEqual(5);
     });
 
+    it('It should have create function', function () {
+        expect(game.Game.create).not.toBe(null);
+    });
+
+    it('It should have preload function', function () {
+        expect(game.Game.preload).not.toBe(null);
+    });
+
+    it('It should have move function', function () {
+        expect(game.Game.moveCharacter).not.toBe(null);
+    });
+
+    it('It should have click function', function () {
+        expect(game.Game.handleClick).not.toBe(null);
+    });
 });
